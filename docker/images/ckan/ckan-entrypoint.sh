b99e9ba0b378ceb289f3057a516b8dc2204e8db9
claravox <claravox@users.noreply.github.com>
2025-11-12 16:04:52 +0100

    Match Lexces more closely to CD2 config

--- docker/images/ckan/ckan-entrypoint.sh
+++ docker/images/ckan/ckan-entrypoint.sh
@@ -34,6 +34,7 @@ check_port redis 6379
 
 ## Initialize CKAN config, database and admin account
 CKAN_CONFIG_FILE=/etc/ckan/default/ckan.ini
+CKAN_UWSGI_CONFIG_FILE=/etc/ckan/default/ckan-uwsgi.ini
 CKAN_INIT_STATUS_FILE=/etc/ckan/default/.ckan_initialized
 
 if [ "$LEXCES_CATALOG_HOST_PORT" = "443" ]
@@ -44,19 +45,19 @@ fi
 if test -f "$CKAN_INIT_STATUS_FILE"
 then echo "Configuration and database already initialized."
 else echo "Initializing configuration ..."
-     export BEAKER_SESSION_SECRET=$(openssl rand -base64 32)
+     ## TODO replace this
      export SECRET_TOKEN_VALUE=$(openssl rand -base64 32)
      export APP_INSTANCE_UUID=$(uuidgen --name "$LEXCES_CATALOG_HOST" --namespace "@url" --sha1)
-     perl -pi.bak -e '$beaker_session_secret=$ENV{BEAKER_SESSION_SECRET}; s/BEAKER_SESSION_SECRET/$beaker_session_secret/ge' "$CKAN_CONFIG_FILE"
      perl -pi.bak -e '$secret_token=$ENV{SECRET_TOKEN_VALUE}; s/SECRET_TOKEN_VALUE/$secret_token/ge' "$CKAN_CONFIG_FILE"
      perl -pi.bak -e '$app_instance_uuid=$ENV{APP_INSTANCE_UUID}; s/APP_INSTANCE_UUID/$app_instance_uuid/ge' "$CKAN_CONFIG_FILE"
      perl -pi.bak -e '$ckan_database_password=$ENV{CKAN_DATABASE_PASSWORD}; s/CKAN_DATABASE_PASSWORD/$ckan_database_password/ge' "$CKAN_CONFIG_FILE"
      perl -pi.bak -e '$ckan_site_url=$ENV{CKAN_SITE_URL}; s/CKAN_SITE_URL/$ckan_site_url/ge' "$CKAN_CONFIG_FILE"
+
      echo "Initializing database ..."
      /usr/lib/ckan/default/bin/ckan -c "$CKAN_CONFIG_FILE" db init
      /usr/lib/ckan/default/bin/ckan -c "$CKAN_CONFIG_FILE" user add ckanadmin password="$CKAN_ADMIN_PASSWORD" email=ckanadmin@localhost name=ckanadmin
      /usr/lib/ckan/default/bin/ckan -c "$CKAN_CONFIG_FILE" sysadmin add ckanadmin
-     chown -R www-data:www-data "$CKAN_STORAGE_PATH"
+     sudo chown -R www-data:www-data "$CKAN_STORAGE_PATH"
      touch "$CKAN_INIT_STATUS_FILE"
      echo "Configuration and database initialization finished."
 fi
